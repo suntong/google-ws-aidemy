0866cc646d1c4c709a1c2e3b68c6c283c4caea82
Christina Lin <weimeilin@gmail.com>
2025-02-20 13:46:45 -0500

    Update book.py

--- planner/book.py
+++ planner/book.py
@@ -4,6 +4,6 @@ from langchain_google_vertexai import VertexAI
 
 
 # Connect to resourse needed from Google Cloud
-llm = VertexAI(model_name="gemini-1.5-flash-002")
+llm = VertexAI(model_name="gemini-1.5-flash-002", location="us-east1")
 BOOK_PROVIDER_URL =  os.environ.get("BOOK_PROVIDER_URL")
 
