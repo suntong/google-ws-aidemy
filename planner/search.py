084cc74c25a3263cfe95738cc19d2994e2c480cb
Christina Lin <weimeilin@gmail.com>
2025-02-20 10:39:10 -0500

    Update search.py

--- planner/search.py
+++ planner/search.py
@@ -4,6 +4,4 @@ from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
 
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")  # Get project ID from env
 LOCATION = os.environ.get("GOOGLE_CLOUD_REGION", "us-central1")
-client = genai.Client(vertexai=True, project=PROJECT_ID, location=LOCATION)
 
-model_id = "gemini-2.0-flash-001"
