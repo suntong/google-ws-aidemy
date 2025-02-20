3ac8f63f99650f24ab3f52f9703ffc559c8c9cdd
Christina Lin <weimeilin@gmail.com>
2025-02-20 10:22:39 -0500

    Update curriculums.py

--- planner/curriculums.py
+++ planner/curriculums.py
@@ -7,6 +7,7 @@ import pg8000
 
 project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
 location = os.environ.get("GOOGLE_CLOUD_REGION", "us-central1")
+instance_name = os.environ.get("INSTANCE_NAME") 
 instance_connection_name = f"{project_id}:{location}:{instance_name}"
 print(f"--------------------------->Instance connection name: {instance_connection_name}")
 
