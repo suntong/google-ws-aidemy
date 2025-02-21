4790b9b8a1e1d9851b3fb6708b19b7718eb6f8a2
Christina Lin <weimeilin@gmail.com>
2025-02-21 16:34:09 -0500

    Add OnRamp workaround

--- portal/render.py
+++ portal/render.py
@@ -6,7 +6,5 @@ from google.cloud import storage
 from langchain_google_vertexai import VertexAI
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
-
-
-llm = VertexAI(model_name="gemini-2.0-flash-thinking-exp-01-21")
+from onramp_workaround import get_next_region
 
