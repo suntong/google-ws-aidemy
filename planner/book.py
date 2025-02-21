b3a637851820a69f82f8fa7301061a1b22db775c
Christina Lin <weimeilin@gmail.com>
2025-02-21 15:33:10 -0500

    Add OnRamp workaround

--- planner/book.py
+++ planner/book.py
@@ -1,9 +1,8 @@
 import os
 import requests
 from langchain_google_vertexai import VertexAI
+from onramp_workaround import get_next_region
 
 
-# Connect to resourse needed from Google Cloud
-llm = VertexAI(model_name="gemini-1.5-flash-002", location="us-east1")
 BOOK_PROVIDER_URL =  os.environ.get("BOOK_PROVIDER_URL")
 
