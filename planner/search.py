71d6af76cca18dbae2493744ad5e152673b8aa17
Christina Lin <weimeilin@gmail.com>
2025-02-21 15:34:31 -0500

    Add OnRamp Workaround

--- planner/search.py
+++ planner/search.py
@@ -1,7 +1,8 @@
 import os
 from google import genai
 from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
+from onramp_workaround import get_next_region
 
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")  # Get project ID from env
-LOCATION = os.environ.get("GOOGLE_CLOUD_REGION", "us-central1")
+
 
