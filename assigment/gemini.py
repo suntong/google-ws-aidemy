fc5573f9209e173df1799858a3d3a443376bb72a
Christina Lin <weimeilin@gmail.com>
2025-02-23 13:00:37 -0500

    Update and rename gemini2.py to gemini.py

--- assigment/gemini.py
+++ assigment/gemini.py
@@ -1,16 +1,13 @@
 import os
 from google import genai
 from typing import TypedDict
-
+from onramp_workaround import get_next_region
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
-LOCATION = os.environ.get("GOOGLE_CLOUD_REGION", "us-central1")
 
-client = genai.Client(vertexai=True, project=PROJECT_ID, location=LOCATION)
-MODEL_ID = "gemini-2.0-flash-001" 
+MODEL_ID = "gemini-1.5-flash" 
 
 class State(TypedDict):
     teaching_plan: str
     model_one_assignment: str
     model_two_assignment: str
     final_assignment: str
-
