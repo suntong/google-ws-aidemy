bbc607345cf03d55f929d27a3aca1fccace36dbb
Christina Lin <weimeilin@gmail.com>
2025-02-23 17:37:02 -0500

    Update render.py

--- portal/render.py
+++ portal/render.py
@@ -7,4 +7,3 @@ from langchain_google_vertexai import VertexAI
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
 from onramp_workaround import get_next_region
-
