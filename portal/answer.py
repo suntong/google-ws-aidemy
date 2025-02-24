af72c46e6b00572359dde48aacafd417ba65329d
Christina Lin <weimeilin@gmail.com>
2025-02-24 10:37:05 -0500

    Update answer.py

--- portal/answer.py
+++ portal/answer.py
@@ -4,3 +4,7 @@ import time
 from langchain_google_vertexai import VertexAI
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
+
+
+def answer_thinking(question, options, user_response, answer, region):
+    return ""
