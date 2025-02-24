bc50a7ec8e599d9e0386ec0e5686a2a946e1f34b
Christina Lin <weimeilin@gmail.com>
2025-02-24 10:38:53 -0500

    Update render.py

--- portal/render.py
+++ portal/render.py
@@ -7,3 +7,6 @@ from langchain_google_vertexai import VertexAI
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
 from onramp_workaround import get_next_region
+
+def render_assignment_page():
+    return ""
